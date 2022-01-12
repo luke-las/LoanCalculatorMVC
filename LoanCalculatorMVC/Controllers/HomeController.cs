a26e5b8f3cfcd5f4466aabc68437ff9719abc148
luke-las <84208824+luke-las@users.noreply.github.com>
2022-01-12 11:47:40 -0500

    Initial commit

---
+++ LoanCalculatorMVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using LoanCalculatorMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using LoanCalculatorMVC.Helper;
+
+namespace LoanCalculatorMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LCPage()
+        {
+            Loan model = new();
+             
+            model.Amount = 15000m;
+            model.Rate = 3.5m;
+            model.TotalInterest = 0.0m;
+            model.TotalCost = 0.0m;
+            model.Payment = 0.0m;
+            model.Term = 60;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult LCPage(Loan loan)
+        {
+            var loanHelper = new LoanHelper();
+            Loan newloan = loanHelper.GetPayments(loan);
+            return View(newloan);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoanCalculatorMVC/Helper/LoanHelper.cs
@@ -0,0 +1,62 @@
+﻿using LoanCalculatorMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanCalculatorMVC.Helper
+{
+    public class LoanHelper
+    {
+        public Loan GetPayments(Loan loan)
+        {
+            //calculate monthly payment
+            int termMonthly = loan.Term;
+            loan.Payment = calcPayment(loan.Amount, loan.Rate, termMonthly);
+
+            //create a loop from 1 to term
+            var balance = loan.Amount;
+            var totalInterest = 0.0m;
+            var monthlyInterest = 0.0m;
+            var monthlyPrincipal = 0.0m;
+            var monthlyRate = CalcMonthlyRate(loan.Rate);
+
+            for (int month = 1; month <= termMonthly; month++)
+            {
+                monthlyInterest = balance * monthlyRate;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                totalInterest += monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                LoanPayment loanPayment = new();
+
+                loanPayment.LoanTerm = month;
+                loanPayment.Payment = loan.Payment;
+                loanPayment.MonthlyInterest = monthlyInterest;
+                loanPayment.MonthlyPrincipal = monthlyPrincipal;
+                loanPayment.TotalInterest = totalInterest;
+                loanPayment.Balance = balance;
+
+                //push object
+                loan.Payments.Add(loanPayment);
+            }
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.Amount + totalInterest;
+
+            //calculate a payment schedule
+            return loan;
+        }
+        private decimal calcPayment(decimal Amount, decimal Rate, int Term)
+        {
+            var rateD = Convert.ToDouble(CalcMonthlyRate(Rate));
+            var amountD = Convert.ToDouble(Amount);
+
+            var paymentD = (amountD * rateD) / (1 - Math.Pow(1 + rateD, -Term));
+            return Convert.ToDecimal(paymentD);
+        }
+        private decimal CalcMonthlyRate(decimal rate)
+        {
+            return (rate / 1200);
+        }
+    }
+}
